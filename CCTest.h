61b26a3ac43f2450a46709dffe074d10d4b746aa
ALAN <yijianxiaodian2008@ichijianxiaoten2008tekiMac.local>
2014-07-01 13:03:16 -0700

    addred a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git test
+//
+//  Created by ALAN on 14-7-1.
+//  Copyright (c) 2014年 ___ZHENGYI___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git test
+//
+//  Created by ALAN on 14-7-1.
+//  Copyright (c) 2014年 ___ZHENGYI___. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
